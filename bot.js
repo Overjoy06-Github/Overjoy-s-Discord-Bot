100d08f50b1514a4d0b633bc85318de2026e0bd0
Overjoy06 <53297297+Overjoy06-Creator@users.noreply.github.com>
2020-05-18 12:33:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ cmds.profile = (msg, args) => {
     .setThumbnail(user.displayAvatarURL())
     .setDescription(`${member} [${displayName}]`)
     .addField("Roles", roles.trim() == "" ? "No roles" : roles)
-    .addField("Joined At : ", msg.member.joinedAt)
+    .addField("Joined At : ", msg.member.joinedTimeStamp)
     .setColor(0xccff00);
 
   getChannel(msg.channel.name, msg.guild).send(embed);
