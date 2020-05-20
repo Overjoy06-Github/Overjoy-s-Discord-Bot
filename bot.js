3fa0c3932ed5fa46c2f1099742974fa98010d77b
Seniru Pasan Indira <senirupasan@gmail.com>
2020-05-20 15:45:35 +0530

    Add a self role system

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 const dotenv = require("dotenv").config();
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const api = require('imageapi.js');
-const superagent = require('superagent')
+const api = require("imageapi.js");
+const superagent = require("superagent");
 
-
-const bot = new Discord.Client({ disableEveryone: true });
+const bot = new Discord.Client({
+  disableEveryone: true,
+  partials: ["REACTION", "MESSAGE"]
+});
 const prefix = botconfig.prefix;
 
 /** Helpful functions*/
@@ -20,7 +22,7 @@ cmds.ping = msg => {
   msg.reply("Pong!");
 };
 
-cmds.meme = async (msg) => {
+cmds.meme = async msg => {
   let subreddits = [
     "comedyheaven",
     "dankmeme",
@@ -29,63 +31,61 @@ cmds.meme = async (msg) => {
     "meme",
     "MemeEconomy",
     "Memes_Of_The_Dank"
-  ]
-  let subreddit = subreddits[Math.floor(Math.random() * subreddits.length - 1)]
-  console.log(subreddit)
-  let img = await api(subreddit)
-  console.log(img)
-  if(!{img}) return msg.reply('I broke please try again!')
+  ];
+  let subreddit = subreddits[Math.floor(Math.random() * subreddits.length - 1)];
+  console.log(subreddit);
+  let img = await api(subreddit);
+  console.log(img);
+  if (!{ img }) return msg.reply("I broke please try again!");
   let Embed = new Discord.MessageEmbed()
     .setTitle(`A meme from r/${subreddit}`)
     .setURL(`https://reddit.com/r/${subreddit}`)
     .setColor(0x00ff00)
-    .setImage(img)
-  
-    getChannel(msg.channel.name, msg.guild).send(Embed);  
+    .setImage(img);
+
+  getChannel(msg.channel.name, msg.guild).send(Embed);
 };
 
 cmds.avatar = async (msg, args) => {
-  let embed = new Discord.MessageEmbed()
-  if(!msg.mentions.users.first()){
-   embed.setTitle(`${msg.author.username}'s Avatar!`)
-   embed.setImage(msg.author.displayAvatarURL())
-   embed.setColor(0x00ff00);
-    
-  }else{
-      let user = msg.mentions.users.first()
-      embed.setTitle(`${user.username}'s Avatar!`)
-      embed.setImage(user.displayAvatarURL())
-      embed.setColor(0x00ff00);
-    
+  let embed = new Discord.MessageEmbed();
+  if (!msg.mentions.users.first()) {
+    embed.setTitle(`${msg.author.username}'s Avatar!`);
+    embed.setImage(msg.author.displayAvatarURL());
+    embed.setColor(0x00ff00);
+  } else {
+    let user = msg.mentions.users.first();
+    embed.setTitle(`${user.username}'s Avatar!`);
+    embed.setImage(user.displayAvatarURL());
+    embed.setColor(0x00ff00);
   }
-  getChannel(msg.channel.name, msg.guild).send(embed)
+  getChannel(msg.channel.name, msg.guild).send(embed);
 };
 
-cmds.dog = async (msg) => {
-  let {body} = await superagent
-    .get('https://dog.ceo/api/breeds/image/random')
-  
-  if(!{body}) return msg.reply('It broke... Please Try Again')
+cmds.dog = async msg => {
+  let { body } = await superagent.get(
+    "https://dog.ceo/api/breeds/image/random"
+  );
+
+  if (!{ body }) return msg.reply("It broke... Please Try Again");
   let embed = new Discord.MessageEmbed()
     .setColor(0x00ff00)
-    .setTitle('Here a cute doggo!')
+    .setTitle("Here a cute doggo!")
     .setImage(body.message)
-    .setTimestamp()
+    .setTimestamp();
 
-    getChannel(msg.channel.name, msg.guild).send({embed})
+  getChannel(msg.channel.name, msg.guild).send({ embed });
 };
 
-cmds.cat = async (msg) => {
-  let {body} = await superagent
-  .get('https://aws.random.cat/meow')
-  if(!{body}) return msg.reply('I broke... Please try again!')
+cmds.cat = async msg => {
+  let { body } = await superagent.get("https://aws.random.cat/meow");
+  if (!{ body }) return msg.reply("I broke... Please try again!");
   let embed = new Discord.MessageEmbed()
     .setColor(0x00ff00)
-    .setTitle('Heres a cute little kitty!')
+    .setTitle("Heres a cute little kitty!")
     .setImage(body.file)
-    .setTimestamp()
-  
-    getChannel(msg.channel.name, msg.guild).send({embed})
+    .setTimestamp();
+
+  getChannel(msg.channel.name, msg.guild).send({ embed });
 };
 
 cmds.profile = (msg, args) => {
@@ -123,9 +123,11 @@ cmds.profile = (msg, args) => {
 cmds.help = msg => {
   let embed = new Discord.MessageEmbed()
     .setTitle("Help :speech_balloon:")
-    .setDescription("**Commands** : \n\n`r!help` - Shows This Embed Message\n`r!profile <player_name>` - Shows The Roles of The Person and Shows Their Profile Picture\n`r!8ball` - Ask 8ball a question and it will answer your question.\n`r!meme` - Generates a random meme.\n`r!dog` - Generates a random dog image.\n`r!cat` - Gemerates a random cat image.\n\n**Mod Commands**\n\n`r!kick <player_name>` - Kicks Person\n`r!ban <player_name>` - Bans Person\n`r!nickname <player_name> <nickname>` - Changes The Person's Nickname.")
+    .setDescription(
+      "**Commands** : \n\n`r!help` - Shows This Embed Message\n`r!profile <player_name>` - Shows The Roles of The Person and Shows Their Profile Picture\n`r!8ball` - Ask 8ball a question and it will answer your question.\n`r!meme` - Generates a random meme.\n`r!dog` - Generates a random dog image.\n`r!cat` - Gemerates a random cat image.\n\n**Mod Commands**\n\n`r!kick <player_name>` - Kicks Person\n`r!ban <player_name>` - Bans Person\n`r!nickname <player_name> <nickname>` - Changes The Person's Nickname."
+    )
     .setColor(0x00ff00);
-  
+
   getChannel(msg.channel.name, msg.guild).send(embed);
 };
 
@@ -135,11 +137,11 @@ cmds.nickname = (msg, args) => {
   if (Discord.MessageMentions.USERS_PATTERN.test(args[0])) {
     if (msg.member.hasPermission(Discord.Permissions.FLAGS.CHANGE_NICKNAME)) {
       let member = msg.mentions.members.first();
-      let nick = args.splice(1).join(" ")
+      let nick = args.splice(1).join(" ");
       member
         .setNickname(nick)
         .then(mem => {
-          msg.reply(`Changed their nickname to ${nick}`)
+          msg.reply(`Changed their nickname to ${nick}`);
         })
         .catch(err => {
           msg.reply("I Can't change nicknames\nReason: `" + err.message + "`");
@@ -148,7 +150,7 @@ cmds.nickname = (msg, args) => {
       msg.reply("u have no perms wat a joke");
     }
   } else {
-    let nick = args.splice(0).join(" ")
+    let nick = args.splice(0).join(" ");
     msg.member
       .setNickname(nick)
       .then(mem => {
@@ -159,6 +161,19 @@ cmds.nickname = (msg, args) => {
       });
   }
 };
+
+/*cmds.role = msg => {
+  let embed = new Discord.MessageEmbed()
+    .setTitle("Server Roles : ")
+    .setColor(0x00ff00)
+    .setDescription("<:roblox:712556824599199785> - Roblox")
+  
+  msg.channel.send({embed: embed}).then(embedMessage => {
+    embedMessage.react('712556824599199785')
+    .then(console.log)
+    .catch(console.error);
+  });
+};*/
 
 //uff
 
@@ -172,14 +187,27 @@ cmds.kick = msg => {
 
 cmds.mute = (msg, args) => {
   if (msg.member.hasPermission(Discord.Permissions.FLAGS.MUTE_MEMBERS)) {
-    msg.reply("OKE")
+    msg.reply("OKE");
   } else {
-    msg.reply("NOOOOOOOOOOO")
+    msg.reply("NOOOOOOOOOOO");
   }
 };
 
-cmds["8ball"] = (msg) => {
-  let random = ["Yes", "No", "What do you think? NO", "Maybe", "Never", "Couldn't agree more", "Of course not", "Of course", "Ask me later", "Ask me again", "Only an idiot would agree", "Can't say i agree"];
+cmds["8ball"] = msg => {
+  let random = [
+    "Yes",
+    "No",
+    "What do you think? NO",
+    "Maybe",
+    "Never",
+    "Couldn't agree more",
+    "Of course not",
+    "Of course",
+    "Ask me later",
+    "Ask me again",
+    "Only an idiot would agree",
+    "Can't say i agree"
+  ];
   let alright = random[Math.floor(Math.random() * random.length)];
   let channel8 = getChannel(msg.channel.name, msg.guild);
   let eightembed = new Discord.MessageEmbed()
@@ -187,8 +215,18 @@ cmds["8ball"] = (msg) => {
     .setTitle(":8ball: 8ball :8ball:")
     .setDescription(`8ball's answer : **${alright}**`)
     .setFooter(msg.author.username);
-  
+
   channel8.send(eightembed);
+};
+
+cmds.ass = msg => {
+  let channel = getChannel("self-role", msg.guild);
+  let embed = new Discord.MessageEmbed()
+    .setColor(0x00ff00)
+    .setTitle("What Game Do You Mainly Play?")
+    .setDescription("<:roblox:712556824599199785> - Roblox");
+
+  channel.send(embed).catch();
 };
 
 cmds.ban = (message, args) => {
@@ -223,6 +261,26 @@ cmds.ban = (message, args) => {
   }
 };
 
+cmds.updateSelfRole = msg => {
+  if (
+    !(
+      msg.author.id == "522972601488900097" ||
+      msg.author.id == "544776631672242176"
+    )
+  )
+    return;
+  let embed = new Discord.MessageEmbed({
+    title: "Self role",
+    description: `<:roblox:712556824599199785> <@&705257970472321094>`,
+    color: 0x00ff00
+  });
+
+  msg.channel.messages.fetch("712591159284727810").then(emsg => {
+    emsg.edit(embed);
+    emsg.react("712556824599199785");
+  });
+};
+
 /** Discord events */
 
 bot.on("ready", async () => {
@@ -233,7 +291,10 @@ bot.on("message", msg => {
   if (msg.author.bot) return;
   if (msg.channel.type == "dm") return;
 
-  if (msg.content.startsWith(prefix) || msg.content.startsWith(prefix.toUpperCase())) {
+  if (
+    msg.content.startsWith(prefix) ||
+    msg.content.startsWith(prefix.toUpperCase())
+  ) {
     let cmdData = msg.content
       .substring(prefix.length)
       .replace(/\s+/g, " ")
@@ -295,6 +356,48 @@ bot.on("messageUpdate", async (oldMsg, newMsg) => {
       .setFooter(`Editted At : ${newMsg.createdAt}`);
 
     channel.send(loggingEmbed);
+  }
+});
+
+bot.on("messageReactionAdd", async (reaction, user) => {
+  if (user.id == bot.user.id) return;
+
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+
+  if (reaction.message.id == "712591159284727810" && reaction.me) {
+    let guild = bot.guilds.cache.find(g => g.id == "691609473252458546");
+    let reactionActions = {
+      "712556824599199785": getRole("Roblox", guild)
+    };
+    if (reactionActions[reaction.emoji.id])
+      guild.member(user).roles.add(reactionActions[reaction.emoji.id]);
+  }
+});
+
+bot.on("messageReactionRemove", async (reaction, user) => {
+  if (user.id == bot.user.id) return;
+
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+
+  if (reaction.message.id == "712591159284727810" && reaction.me) {
+    let guild = bot.guilds.cache.find(g => g.id == "691609473252458546");
+    let reactionActions = {
+      "712556824599199785": getRole("Roblox", guild)
+    };
+    if (reactionActions[reaction.emoji.id])
+      guild.member(user).roles.remove(reactionActions[reaction.emoji.id]);
   }
 });
 
