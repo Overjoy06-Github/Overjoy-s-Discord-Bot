cdaf9e0d5b78a1a3b40eb7412a87b7fb91dc538a
Overjoy06 <53297297+Overjoy06-Creator@users.noreply.github.com>
2020-05-26 18:14:06 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,34 @@ cmds.ping = msg => {
   msg.reply("Pong!");
 };
 
+cmds.rps = async(message, args) => {
+let rps = ["scissors", "paper", "rock"];
+let i;
+if(!rps.includes(args[0])) return message.reply("Please choose rock, paper or scissor.");
+if(args[0].includes("rock")) {
+i = 2;
+}
+if(args[0].includes("paper")) {
+i = 1;
+}
+if(args[0].includes("scissors")) {
+i = 0;
+}
+if(rps[i]) {
+let comp = Math.floor((Math.random() * 3) + 1);
+let comp_res = parseInt(comp) - parseInt("1");
+let comp_val = rps[parseInt(comp_res)];
+  if(i === comp_res) {
+    return message.channel.send(`You chose **${args[0]}** and I chose **${comp_val}** and we tied, wanna try again?`); 
+  }
+  if(i > comp_res) {
+    return message.channel.send(`You chose **${args[0]}** and I chose **${comp_val}** and I won! Well played.`);
+  } 
+  if(i < comp_res) {
+    return message.channel.send(`You chose **${args[0]}** and I chose **${comp_val}** and I lost! Congrats on winning!`);
+    }
+  }
+};
 cmds.purge = async(msg,args) => {
   if (msg.deletable) {
     msg.delete();
@@ -219,7 +247,7 @@ cmds.help = msg => {
   let embed = new Discord.MessageEmbed()
     .setTitle("Help :speech_balloon:")
     .setDescription(
-      "**Commands** : \n\n`r!help` - Shows This Embed Message\n`r!profile <player_name>` - Shows The Roles of The Person and Shows Their Profile Picture\n`r!8ball` - Ask 8ball a question and it will answer your question.\n`r!meme` - Generates a random meme.\n`r!dog` - Generates a random dog image.\n`r!cat` - Gemerates a random cat image.\n`r!avatar <player_name>` - Shows the person's avatar\n\n**Mod Commands**\n\n`r!kick <player_name>` - Kicks Person\n`r!ban <player_name>` - Bans Person\n`r!nickname <player_name> <nickname>` - Changes The Person's Nickname.\n`r!purge <amount>` - Deletes all the previous messages depending on the amount.`"
+      "**Commands** : \n\n`r!help` - Shows This Embed Message\n`r!profile <player_name>` - Shows The Roles of The Person and Shows Their Profile Picture\n`r!8ball` - Ask 8ball a question and it will answer your question.\n`r!meme` - Generates a random meme.\n`r!dog` - Generates a random dog image.\n`r!cat` - Gemerates a random cat image.\n`r!avatar <player_name>` - Shows the person's avatar\n`r!rps <choice>` - Plays a game of rock paper scissors.\n\n**Mod Commands**\n\n`r!kick <player_name>` - Kicks Person\n`r!ban <player_name>` - Bans Person\n`r!nickname <player_name> <nickname>` - Changes The Person's Nickname.\n`r!purge <amount>` - Deletes all the previous messages depending on the amount.`"
     )
     .setColor(0x00ff00);
 
